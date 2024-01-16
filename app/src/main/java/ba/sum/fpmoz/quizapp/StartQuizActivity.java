3a6f85d498476ef5f3f6b97dc86baed7e45778bc
JosipAleric <josip.aleric@fpmoz.sum.ba>
2024-01-16 17:14:42 +0100

    Start quiz aktivnost

---
+++ app/src/main/java/ba/sum/fpmoz/quizapp/StartQuizActivity.java
@@ -0,0 +1,28 @@
+package ba.sum.fpmoz.quizapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class StartQuizActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_quiz);
+
+        Button startQuizBtn = findViewById(R.id.startQuizButton);
+
+        startQuizBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(StartQuizActivity.this, QuizActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
