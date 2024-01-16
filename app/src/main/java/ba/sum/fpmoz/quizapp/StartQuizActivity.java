ae788c1c6bea8fc0e3c989e28104e718f4ae59d2
JosipAleric <josip.aleric@fpmoz.sum.ba>
2024-01-16 17:17:43 +0100

    fix

--- app/src/main/java/ba/sum/fpmoz/quizapp/StartQuizActivity.java
+++ app/src/main/java/ba/sum/fpmoz/quizapp/StartQuizActivity.java
@@ -14,15 +14,15 @@ public class StartQuizActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_quiz);
 
-        Button startQuizBtn = findViewById(R.id.startQuizButton);
-
-        startQuizBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(StartQuizActivity.this, QuizActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        });
+//        Button startQuizBtn = findViewById(R.id.startQuizButton);
+//
+//        startQuizBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(StartQuizActivity.this, QuizActivity.class);
+//                startActivity(intent);
+//                finish();
+//            }
+//        });
     }
 }
